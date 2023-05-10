b8ef8f11eac8e103117ee5f60d462d6b543c9178
Think Stack Limited <29373851+thinkstack@users.noreply.github.com>
2023-05-10 11:50:14 +0100

    TEL-3760 add Muhammed remove Willem (#23)
    Co-authored-by: Muhammed Ahmed <ma3574@users.noreply.github.com>

--- src/helper.py
+++ src/helper.py
@@ -8,11 +8,11 @@ class Helper:
         "gavD@users.noreply.github.com": "gavin.davies1",
         "29373851+thinkstack@users.noreply.github.com": "lee.myring",
         "22219356+matthew-hollick@users.noreply.github.com": "matthew.hollick",
+        "ma3574@users.noreply.github.com": "muhammed.ahmed",
         "66684341+rizinaa99@users.noreply.github.com": "rizina.khatun",
         "Crumplepang@users.noreply.github.com": "rob.white",
         "18111914+sjpalf@users.noreply.github.com": "stephen.palfreyman",
         "67912934+TimothyFothergill@users.noreply.github.com": "timothy.fothergill",
-        "6502426+willemveerman@users.noreply.github.com": "willem.veerman2",
     }
 
     def __init__(
